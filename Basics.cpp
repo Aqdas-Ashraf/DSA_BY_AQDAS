8febd5014306cd1a7f5f5155074017b08f752f34
Aqdas Ashraf <aqdasashraf01041547@gmail.com>
2026-08-28 19:56:22 +0530

    Initial commit

---
+++ Basics.cpp
@@ -0,0 +1,62 @@
+// #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int x,y;
+    // cin >> x >> y;
+    // cout<<"x = " << x << " and y = " << y;
+
+    // string str;
+    // String used for multiple character 
+    // cin>>str;
+    // char c;
+     // uses only for one character
+    // getline(cin , str);
+    // cin>>c;
+    // cout<<c;
+
+    // int grade;
+    // cout<<"Enter your grade = ";
+    // cin>>grade;
+
+    // if(grade<25){
+    //     cout<<"F";
+    // }
+    // if(grade<=44 && grade>=25){
+    //     cout<<"E";
+    // }
+    // if(grade>=45 && grade<=49){
+    //     cout<<"D";
+    // }
+    // if(grade>=50 && grade<=59){
+    //     cout<<"C";
+    // }
+    // if(grade>=60 && grade<=79){
+    //     cout<<"B";
+    // }
+    // if(grade>=80 && grade <=100){
+    //     cout<<"A";
+    // }
+
+//     if(grade<=25){
+//         cout<<"F";
+//     }
+//     else if(grade<=44){
+// cout<<"E";
+//     }
+//     else if(grade<=49){
+// cout<<"D";
+//     }
+//     else if(grade<=59){
+// cout<<"C";
+//     }
+//     else if(grade<=79){
+// cout<<"B";
+//     }
+//     else if(grade<=100){
+// cout<<"A";
+//     }
+
+    return 0;
+}
