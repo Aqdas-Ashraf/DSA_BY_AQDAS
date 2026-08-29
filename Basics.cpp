276b7e570a6e54b6f5189b58d2e57ef39a583be7
Aqdas Ashraf <aqdasashraf01041547@gmail.com>
2026-08-29 14:47:43 +0530

    length

--- Basics.cpp
+++ Basics.cpp
@@ -67,36 +67,40 @@ int main(){
 //         cout<<"Who r u";
 //     }
 
-int day;
-cout<<"Enter the day";
-cin>>day;
+// int day;
+// cout<<"Enter the day";
+// cin>>day;
 
-switch (day)
-{
-case 1:
-    cout<<"Monday";
-    break;
-case 2:
-cout<<"Tuesday";
-    break;
-case 3:
-cout<<"Wednesday";
-    break;
-case 4:
-cout<<"Thursday";
-    break;
-case 5:
-cout<<"Friday";
-    break;
-case 6:
-cout<<"Saturday";
-    break;
-case 7:
-cout<<"Sunday";
-    break;
-default:
+// switch (day)
+// {
+// case 1:
+//     cout<<"Monday";
+//     break;
+// case 2:
+// cout<<"Tuesday";
+//     break;
+// case 3:
+// cout<<"Wednesday";
+//     break;
+// case 4:
+// cout<<"Thursday";
+//     break;
+// case 5:
+// cout<<"Friday";
+//     break;
+// case 6:
+// cout<<"Saturday";
+//     break;
+// case 7:
+// cout<<"Sunday";
+//     break;
+// default:
     // cout<<"invalid";
-}
+// }
+
+ string s = "Aqdas";
+ int len = s.size();  
+ cout<<s[len-2];
 
 
     return 0;
