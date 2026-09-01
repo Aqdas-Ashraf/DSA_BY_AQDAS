2632cad5f5e44751630c9e01f512e9779f97c557
Aqdas Ashraf <aqdasashraf01041547@gmail.com>
2026-09-01 13:57:24 +0530

    Basics

---
+++ Basics/Basics.cpp
@@ -0,0 +1,155 @@
+// #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void printname(string dep , string name, int age){
+    cout<<"My age is :"<<age<<endl;
+    cout<<"My name is "<<name<<endl;
+    cout<<"My dep is "<<dep;
+}
+
+int main(){
+    // int x,y;
+    // cin >> x >> y;
+    // cout<<"x = " << x << " and y = " << y;
+
+    // string str;
+    // String used for multiple character 
+    // cin>>str;
+    // char c;
+     // uses only for one character
+    // getline(cin , str);
+    // cin>>c;
+    // cout<<c;
+
+    // int grade;
+    // cout<<"Enter your grade = ";
+    // cin>>grade;
+
+    // if(grade<25){
+    //     cout<<"F";
+    // }
+    // if(grade<=44 && grade>=25){
+    //     cout<<"E";
+    // }
+    // if(grade>=45 && grade<=49){
+    //     cout<<"D";
+    // }
+    // if(grade>=50 && grade<=59){
+    //     cout<<"C";
+    // }
+    // if(grade>=60 && grade<=79){
+    //     cout<<"B";
+    // }
+    // if(grade>=80 && grade <=100){
+    //     cout<<"A";
+    // }
+
+//     if(grade<=25){
+//         cout<<"F";
+//     }
+//     else if(grade<=44){
+// cout<<"E";
+//     }
+//     else if(grade<=49){
+// cout<<"D";
+//     }
+//     else if(grade<=59){
+// cout<<"C";
+//     }
+//     else if(grade<=79){
+// cout<<"B";
+//     }
+//     else if(grade<=100){
+// cout<<"A";
+//     }
+
+//  bool isAqdas = true;
+
+//     if(isAqdas){
+//         cout<<"hey Aqdas";
+//     }
+//     else{
+//         cout<<"Who r u";
+//     }
+
+// int day;
+// cout<<"Enter the day";
+// cin>>day;
+
+// switch (day)
+// {
+// case 1:
+//     cout<<"Monday";
+//     break;
+// case 2:
+// cout<<"Tuesday";
+//     break;
+// case 3:
+// cout<<"Wednesday";
+//     break;
+// case 4:
+// cout<<"Thursday";
+//     break;
+// case 5:
+// cout<<"Friday";
+//     break;
+// case 6:
+// cout<<"Saturday";
+//     break;
+// case 7:
+// cout<<"Sunday";
+//     break;
+// default:
+    // cout<<"invalid";
+// }
+
+//  string s = "Aqdas";
+//  int len = s.size();  
+//  s[len-2] = 'e';
+//  cout<<s;
+// int i;
+// for(i=0;i<=5;i = i+4){
+//     cout<<"Aqdas\n";
+// }
+// cout<<i;
+
+// int i = 10;
+// while(i<=20){
+//     cout<<"Aqdas ashraf\n";
+//     i = i+2;
+// }
+
+
+// int i=0;;
+// do{
+//     cout <<"Aqdas here"<<endl;
+//     i++;
+// }
+// while(i<=5);
+
+
+// funcations are set of code which perform somethings for you
+// fucntions are used to modularise code
+// functions are used to increase readability
+// functions are used to use same code multiple times
+/*
+functions we use :
+void , return , parameterised , non parametarised
+*/
+
+string name;
+string dep;
+int age;
+cout<<"Enter your age :";
+cin>>age;
+cin.ignore();
+cout<<"Enter your name : ";
+getline(cin,name);
+cout<<"Enter your dep : ";
+getline(cin,dep);
+printname(dep,name,age);
+
+
+    return 0;
+}
